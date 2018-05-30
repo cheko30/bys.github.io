8897961f9b17bda8d0d8b49680b3ba1042efca9f
Cheko <sergio.pena.orozco@gmail.com>
2018-05-30 09:31:17 -0500

    Initial commit

---
+++ exsythemes/static/js/main.js
@@ -0,0 +1,7 @@
+/* Preloader js */
+$(window).on('load', function() { 
+   "use strict";   
+    $('.status').fadeOut();
+    $('#preloader').delay(350).fadeOut('slow'); 
+}); 
+/* END Preloader js */
